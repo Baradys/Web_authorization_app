684fba654be6ab341fdb72691cfa89e3f2c2f850
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-12 22:13:48 +0300

    Project is ready to test

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
 explorer http://localhost:8080/
 @echo off
-java -jar out/artifacts/WebProject_jar/Web_authorization_app.jar
+java -jar out/artifacts/Web_authorization_app_jar/Web_authorization_app.jar
 @pause
