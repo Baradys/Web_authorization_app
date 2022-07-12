27a77dbb77e0568f7716f41bc5bb80d38248f7cb
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-12 21:54:12 +0300

    Added class to run server.

---
+++ src/main/java/ru/baradys/web_app/controllers/MainController.java
@@ -0,0 +1,10 @@
+package ru.baradys.web_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/")
+@Controller
+public class MainController {
+}
